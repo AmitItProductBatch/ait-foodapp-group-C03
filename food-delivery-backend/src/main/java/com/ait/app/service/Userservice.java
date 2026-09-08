c24e31ed430dc075fce507021e03afabecad99a6
Aniket-Ashtekar <aniketashtekar2366@gmail.com>
2026-09-08 15:23:27 +0530

    OFD-103D RESEND

--- food-delivery-backend/src/main/java/com/ait/app/service/Userservice.java
+++ food-delivery-backend/src/main/java/com/ait/app/service/Userservice.java
@@ -1,14 +1,4 @@
-/*package com.ait.app.service;
 
-import com.ait.app.dto.UserRequestDTO;
-import com.ait.app.entity.User;
-
-public interface Userservice {
-
-	User Registeruser(UserRequestDTO dto);
-
-}
-*/
 package com.ait.app.service;
 
 import com.ait.app.dto.LoginRequestDTO;
--- food-delivery-backend/src/main/java/com/ait/app/serviceimpl/UserServiceimpl.java
+++ food-delivery-backend/src/main/java/com/ait/app/serviceimpl/UserServiceimpl.java
@@ -1,34 +1,4 @@
-/*package com.ait.app.serviceimpl;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import com.ait.app.dto.UserRequestDTO;
-import com.ait.app.entity.User;
-import com.ait.app.repository.UserRepository;
-import com.ait.app.service.Userservice;
-
-@Service
-public class UserServiceimpl implements Userservice {
-
-	@Autowired
-	UserRepository repository;
-
-	@Override
-	public User Registeruser(UserRequestDTO dto) {
-
-		User user = new User();
-
-		user.setName(dto.getName());
-		user.setEmail(dto.getEmail());
-		user.setPassword(dto.getPassword());
-		user.setPhonenumber(dto.getPhonenumber());
-		user.setRole(dto.getRole());
-
-		return repository.save(user);
-	}
-
-}*/
 package com.ait.app.serviceimpl;
 
 import org.springframework.beans.factory.annotation.Autowired;
