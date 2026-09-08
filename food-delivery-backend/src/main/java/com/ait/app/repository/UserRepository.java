06d143982da03fa6d9881cad2676f6bbdee5d7a4
Aniket-Ashtekar <aniketashtekar2366@gmail.com>
2026-09-08 14:43:05 +0530

    OFD-103 DONE DONT MERGE ITS TESTING PERPOSE

--- food-delivery-backend/src/main/java/com/ait/app/repository/UserRepository.java
+++ food-delivery-backend/src/main/java/com/ait/app/repository/UserRepository.java
@@ -1,9 +1,23 @@
-package com.ait.app.repository;
+/*package com.ait.app.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ait.app.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+
+}
+*/
+package com.ait.app.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ait.app.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByEmail(String email);
 
 }
