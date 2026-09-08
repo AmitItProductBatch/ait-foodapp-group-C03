6d811f49d707be4f57ff1b721b8163466588b2e3
AbhijeetJoshi45 <abhijeetjoshi63@gmail.com>
2026-09-08 13:15:12 +0530

    hi
    h88

--- food-delivery-backend/src/main/java/com/ait/app/controller/UserController.java
+++ food-delivery-backend/src/main/java/com/ait/app/controller/UserController.java
@@ -19,6 +19,12 @@ public class UserController {
 	public User registerUser(@RequestBody UserRequestDTO dto) {
 
 		return userservice.Registeruser(dto);
+<<<<<<< Updated upstream
+=======
+		
+		
+	
+>>>>>>> Stashed changes
 	}
 
 }
