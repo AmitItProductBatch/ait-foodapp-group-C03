44c7b9e229d139c7558926d7cf96d8b05f26ce2d
AbhijeetJoshi45 <abhijeetjoshi63@gmail.com>
2026-09-11 20:55:44 +0530

    OFO-101
    fix

--- food-delivery-backend/src/main/java/com/ait/app/serviceimpl/UserServiceimpl.java
+++ food-delivery-backend/src/main/java/com/ait/app/serviceimpl/UserServiceimpl.java
@@ -1,30 +1,141 @@
 package com.ait.app.serviceimpl;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.ait.app.dto.AdressUpdateDto;
+import com.ait.app.dto.LoginRequestDTO;
+import com.ait.app.dto.UpdateProfileDto;
 import com.ait.app.dto.UserRequestDTO;
+import com.ait.app.entity.Address;
 import com.ait.app.entity.User;
+import com.ait.app.repository.AddressRepository;
 import com.ait.app.repository.UserRepository;
 import com.ait.app.service.Userservice;
 
 @Service
-public class UserServiceimpl implements Userservice{
-@Autowired
-UserRepository repository;
+public class UserServiceimpl implements Userservice {
 
- @Override
-	public User Registeruser(UserRequestDTO dto) {
-		// TODO Auto-generated method stub
-	 User user = new User();
-	 user.setName(dto.getName());
-	 user.setAddress(dto.getAddress());
-	 user.setEmail(dto.getEmail());
-	 user.setPassword(dto.getPassword());
-	 user.setPhonenumber(dto.getPhonenumber());
-	 user.setRole(dto.getRole());
-	 
-		return repository.save(user);
-	}
+    @Autowired
+    UserRepository repository;
 
+    @Autowired
+    AddressRepository addressRepository;
+
+    @Override
+    public User Registeruser(UserRequestDTO dto) {
+
+        User user = new User();
+
+        user.setName(dto.getName());
+        user.setEmail(dto.getEmail());
+        user.setPassword(dto.getPassword());
+        user.setPhonenumber(dto.getPhonenumber());
+        user.setRole(dto.getRole());
+
+        return repository.save(user);
+    }
+
+    @Override
+    public User login(LoginRequestDTO dto) {
+
+        Optional<User> optionalUser = repository.findByEmail(dto.getEmail());
+
+        if (!optionalUser.isPresent()) {
+            throw new RuntimeException("Invalid email or password");
+        }
+
+        User user = optionalUser.get();
+
+        if (!user.getPassword().equals(dto.getPassword())) {
+            throw new RuntimeException("Invalid email or password");
+        }
+
+        return user;
+    }
+
+    @Override
+    public User getUser(int id) {
+
+        Optional<User> optionalUser = repository.findById(id);
+
+        if (!optionalUser.isPresent()) {
+            throw new RuntimeException("User not found");
+        }
+
+        return optionalUser.get();
+    }
+
+    @Override
+    public User updateProfile(int id, UpdateProfileDto dto) {
+
+        Optional<User> optionalUser = repository.findById(id);
+
+        if (!optionalUser.isPresent()) {
+            throw new RuntimeException("User not found");
+        }
+
+        User user = optionalUser.get();
+
+       
+        if (dto.getName() != null) {
+            user.setName(dto.getName());
+        }
+
+       
+        if (dto.getPhonenumber() != null) {
+            user.setPhonenumber(dto.getPhonenumber());
+        }
+
+        
+        if (dto.getAdressUpdateDto() != null) {
+
+            AdressUpdateDto addressDto = dto.getAdressUpdateDto();
+
+            Optional<Address> optionalAddress =
+                    addressRepository.findByIdAndUserId(
+                            addressDto.getId(), id);
+
+            if (!optionalAddress.isPresent()) {
+                throw new RuntimeException("Address not found");
+            }
+
+            Address address = optionalAddress.get();
+
+            if (addressDto.getAddressLabel() != null) {
+                address.setAddressLabel(addressDto.getAddressLabel());
+            }
+
+            if (addressDto.getStreetAddress() != null) {
+                address.setStreetAddress(addressDto.getStreetAddress());
+            }
+
+            if (addressDto.getApartment() != null) {
+                address.setApartment(addressDto.getApartment());
+            }
+
+            if (addressDto.getLandmark() != null) {
+                address.setLandmark(addressDto.getLandmark());
+            }
+
+            if (addressDto.getCity() != null) {
+                address.setCity(addressDto.getCity());
+            }
+
+            if (addressDto.getPostalCode() != null) {
+                address.setPostalCode(addressDto.getPostalCode());
+            }
+
+            if (addressDto.getDeliveryInstructions() != null) {
+                address.setDeliveryInstructions(
+                        addressDto.getDeliveryInstructions());
+            }
+
+            addressRepository.save(address);
+        }
+
+        return repository.save(user);
+    }
 }
