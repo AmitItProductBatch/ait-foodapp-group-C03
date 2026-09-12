5eb2d6e792df824c90ca2e202a006ec5d87fc118
shivendra <shivendrakanade2004@gmail.com>
2026-09-12 17:51:52 +0530

    OFO-115
    View Item Price Task Completed.

--- food-delivery-backend/src/main/java/com/ait/app/service/MenuItemService.java
+++ food-delivery-backend/src/main/java/com/ait/app/service/MenuItemService.java
@@ -2,9 +2,11 @@ package com.ait.app.service;
 
 import com.ait.app.dto.MenuItemRequestDTO;
 import com.ait.app.dto.MenuItemResponseDTO;
+import com.ait.app.dto.PriceResponseDTO;
 
 public interface MenuItemService {
 
 	MenuItemResponseDTO createMenuItem(int restaurantId, MenuItemRequestDTO requestDTO);
 
+	PriceResponseDTO getItemPrice(int itemId);
 }
